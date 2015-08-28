2b912fcf59448a9a15322dd3199ae329447105a5
ricardobortoti <ricardobortoti@gmail.com>
2015-08-28 17:45:49 -0300

    GameOfLife

---
+++ Moderate/GameOfLife/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+
+        const int griddimension = 10;
+
+        static void Main(string[] args)
+        {
+            int[,] grid = new int[griddimension, griddimension];
+            int[,] newgrid = new int[griddimension, griddimension];
+
+            using (StreamReader reader = File.OpenText("C:\\TESTE.TXT"))
+            {
+                int line = 0;
+
+                string linetext;
+
+                while (!reader.EndOfStream)
+                {
+                    linetext = reader.ReadLine();
+
+                    for (int col = 0; col < griddimension; col++)
+                    {
+                        grid[line, col] = linetext[col] == '.' ? 0 : 1;
+                    }
+
+                    line++;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
